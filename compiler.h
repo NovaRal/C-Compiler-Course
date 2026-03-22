14b5ae88497aabd3228f1b3d6717e90d75f1ca0d
NovaRal <kmvs1320@gmail.com>
2026-03-23 00:53:44 +0530

    Added basic structs and enums for token, location of token and type of token

--- compiler.h
+++ compiler.h
@@ -2,6 +2,50 @@
 #define PEACHCOMPILER_H
 
 #include <stdio.h>
+#include <stdbool.h>
+
+struct pos
+{
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum
+{
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union
+    {
+        char cval;
+        const char* sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+
+    // True if whitespace between token and prev token
+    // ex:- + a for operator + means whitespace would be set for token "a"
+    bool whitespace;
+
+    // (Hello World)
+    // ^ pointing to the bracket there
+    const char* between_brackets;
+}
 
 enum
 {
