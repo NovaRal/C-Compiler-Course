db05b8c182e43df68ab4a6ca58832ca781fc534c
NovaRal <kmvs1320@gmail.com>
2026-03-23 01:33:02 +0530

    Added functionality of compiler process functions and interfaces for lexer process

--- compiler.h
+++ compiler.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// TOKEN STRUCTS
 struct pos
 {
     int line;
@@ -47,6 +48,39 @@ struct token
     const char* between_brackets;
 };
 
+// LEXER PROCESS INTERFACE
+struct lex_process;
+typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process* process);
+typedef char (*LEX_PROCESS_PEEK_CHAR)(struct lex_process* process);
+typedef void (*LEX_PROCESS_PUSH_CHAR)(struct lex_process* process, char c);
+
+struct lex_process_functions
+{
+    LEX_PROCESS_NEXT_CHAR next_char;
+    LEX_PROCESS_PEEK_CHAR peek_char;
+    LEX_PROCESS_PUSH_CHAR push_char;
+};
+
+struct lex_process
+{
+    struct pos pos;
+    struct vector* token_vec;
+    struct compile_process* compiler;
+
+    /**
+     * depth of bracket count
+     * ex: ((abc))
+     * count = 2;
+     */
+    int curr_expression_count;
+    struct buffer* parantheses_buffer;
+    struct lexprocess_functions* function;
+
+    // Private Data which lexer does not understand but user using lexer does.
+    void* private;
+};
+
+// COMPILE PROCESS INTERFACE
 enum
 {
     COMPILER_FILE_COMPILED_OK,
@@ -58,6 +92,7 @@ struct compile_process
     // Flags to decide the compilation process
     int flags;
 
+    struct pos pos;
     struct compile_process_input_file
     {
         FILE* fp;
@@ -70,4 +105,7 @@ struct compile_process
 int compile_file(const char* filename, const char* out_filename, int flags);
 struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags);
 
+char compile_process_next_char(struct lex_process* lex_process);
+char compile_process_peek_char(struct lex_process* lex_process);
+void compile_process_push_char(struct lex_process* lex_process, char c);
 #endif
